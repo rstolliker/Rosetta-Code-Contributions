ba604b41ad0d53b1182baadb2708ee2ac669b3f8
Ryan Stolliker <ryan.stolliker@gmail.com>
2016-09-20 02:12:02 -0700

    Declarations of functions

--- AVL Tree/avl-tree.ts
+++ AVL Tree/avl-tree.ts
@@ -11,10 +11,55 @@ class AVLnode <T> {
     }
 }
 
-/** Interface for the deciding function in the AVL tree */
-interface DecisionFunction <T> {
-    (first: T, second: T): boolean
+/** A hack to emulate a pointer to AVLnode.
+ * Needed because javascript passes references by value only.
+ * While most operations will work, they would not be saved outside the function scope.
+ * Also, 'this' cannot be reassigned.
+ */
+class nodePtr<T> {
+    constructor(public node: AVLnode<T> = null) {}
 }
 
 /** The balanced AVL tree */
-class AVLtree <T> {}
+class AVLtree <T> {
+    // public members organized here
+    constructor() {}
+
+    insert(key: T): boolean {}
+
+    deleteKey(key: T): void {}
+
+    printTreeBalance(): void {}
+
+
+    // private members organized here
+    private root: nodePtr<T>
+
+    private rotateLeft(a: nodePtr<T>): nodePtr<T> {}
+
+    private rotateRight(a: nodePtr<T>): nodePtr<T> {}
+
+    private rotateLeftThenRight(n: nodePtr<T>): nodePtr<T> {}
+
+    private rotateRightThenLeft(n: nodePtr<T>): nodePtr<T> {}
+
+    private rebalance(n: nodePtr<T>): void {
+        this.setBalance(n)
+
+        if (n.node.balance === -2) {
+            if(this.height(n.node.left.left) >= this.height(n.node.left.right)) {
+                n.node = this.rotateRight(n).node
+            } else {
+                n.node = this.rotateLeftThenRight(n).node
+            }
+        }
+    }
+
+    private height(n: AVLnode<T>): number {}
+
+    private setBalance(n: AVLnode<T>): void {}
+
+    private printNodeBalance(n: AVLnode<T>): void {}
+
+    private clearNode(n: AVLnode<T>): void {}
+}
