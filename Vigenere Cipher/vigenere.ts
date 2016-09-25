90cfea9fa0b5a5fc91eb8f098ec64d1caa02ac17
Ryan Stolliker <ryan.stolliker@gmail.com>
2016-09-25 16:44:05 -0700

    Finished

--- Vigenere Cipher/vigenere.ts
+++ Vigenere Cipher/vigenere.ts
@@ -8,13 +8,29 @@ class Vigenere {
         for(let i = 0; i < plainText.length; ++i) {
             let char = plainText.charAt(i)
             if (char >= 'A' && char <= 'Z') {
-                result += () % 26
+                result += String.fromCharCode((char.charCodeAt(0) + this.key.charCodeAt(i % this.key.length) - 130) % 26 + 65)
+            } else {
+                result += char
             }
         }
+        return result
     }
 
-    decrypt(cipherText: string): string {}
+    decrypt(cipherText: string): string {
+        cipherText = cipherText.toUpperCase()
+        let result: string = ""
+        for(let i = 0; i < cipherText.length; ++i) {
+            let char = cipherText.charAt(i)
+            if (char >= 'A' && char <= 'Z') {
+                result += String.fromCharCode((char.charCodeAt(0) - this.key.charCodeAt(i % this.key.length)) % 26 + 65)
+            } else {
+                result += char
+            }
+        }
+        return result
+    }
 
     private static charNum(char: string): number {}
 
 }
+
